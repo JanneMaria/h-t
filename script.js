3d8ec0aacbeb73ed2b483238d711ec9735fe5740
JanneMaria <157970522+JanneMaria@users.noreply.github.com>
2024-01-28 16:15:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const animationContainer = document.getElementById('animationContainer');
+
+    setTimeout(function () {
+        // Add the 'show-animation' class after a delay (e.g., 2 seconds)
+        animationContainer.classList.add('show-animation');
+
+        // Remove the animation container after another delay (e.g., 5 seconds)
+        setTimeout(function () {
+            animationContainer.remove();
+        }, 10000); // Adjust the delay for removal as needed
+    }, 1000); // Adjust the delay for showing the animation as needed
+});
+
